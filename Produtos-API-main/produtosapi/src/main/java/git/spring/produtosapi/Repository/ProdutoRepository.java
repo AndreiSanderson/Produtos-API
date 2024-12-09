500b7296a19d705d500858b1ca836e4450f843a6
Sanderson <andreisanderson37@gmail.com>
2024-12-09 19:33:12 -0400

    Produtos API - 02

--- Produtos-API-main/produtosapi/src/main/java/git/spring/produtosapi/Repository/ProdutoRepository.java
+++ Produtos-API-main/produtosapi/src/main/java/git/spring/produtosapi/Repository/ProdutoRepository.java
@@ -1,9 +1,13 @@
 package git.spring.produtosapi.Repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import git.spring.produtosapi.model.Produto;
 
+
 public interface ProdutoRepository extends JpaRepository<Produto, String> {
 
+    List<Produto> findByNome(String nome);
 }
--- Produtos-API-main/produtosapi/src/main/java/git/spring/produtosapi/controller/ProdutoControler.java
+++ Produtos-API-main/produtosapi/src/main/java/git/spring/produtosapi/controller/ProdutoControler.java
@@ -1,10 +1,16 @@
 package git.spring.produtosapi.controller;
 
+import java.util.List;
 import java.util.UUID;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import git.spring.produtosapi.Repository.ProdutoRepository;
@@ -30,4 +36,28 @@ public class ProdutoControler {
         produtoRepository.save(produto);
         return produto;
     }
+
+    @GetMapping("/{id}")
+    public Produto obterPorId(@PathVariable("id") String id){
+        return produtoRepository.findById(id).orElse(null);
+        //metodo opcional...
+        //Optional<Produto> byId = produtoRepository.findById(id);
+        //return produto.isPresent() ? produto.get() : null;
+    }
+
+    @DeleteMapping("{id}")
+    public void deletar(@PathVariable("id") String id){
+        produtoRepository.deleteById(id);
+    }
+
+    @PutMapping("{id}")
+    public void atualizar(@PathVariable("id") String id, @RequestBody Produto produto){
+        produto.setId(id);
+        produtoRepository.save(produto);
+    }
+
+    @GetMapping
+    public List<Produto> buscar(@RequestParam("nome") String nome){
+        return produtoRepository.findByNome(nome);
+    } 
 }
